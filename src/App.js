dff821b0cd0fd9788e92dcf16c8c8ae48d1237f6
emanuelklado <emanuelklado@msn.com>
2022-02-26 00:59:11 -0300

    requisito 09 ok nos testes

--- src/App.js
+++ src/App.js
@@ -93,6 +93,7 @@ class App extends React.Component {
   removeCard(cardName) {
     const { data } = this.state;
     this.setState(({ data: data.filter((el) => el.cardName !== cardName) }));
+    this.checkHasTrunfo();
   }
 
   render() {
